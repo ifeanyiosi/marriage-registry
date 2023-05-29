bac94903ef802c66f4edadd7fbbc569735667944
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2023-05-29 15:13:06 +0100

    installed dependencies i would need for SIgn up and Login Form

---
+++ src/pages/dashboard/addons/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Addons = (props: Props) => {
+  return <div>Addons</div>;
+};
+
+export default Addons;
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return <div className="w-full bg-red-400 h-full mt-[80px] ">Dashboard</div>;
+};
+
+export default Dashboard;
